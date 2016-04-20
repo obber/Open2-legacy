5f25ef4d404d637302ffed423d7a080a6a392467
Xiaolu Bai <icebunnydoll@gmail.com>
2016-04-20 15:45:56 -0700

    [Feature] Log out button working

--- client/app/services.js
+++ client/app/services.js
@@ -28,22 +28,24 @@ angular
       function err(err){
         $location.path('/login');
         return console.log(err);
-      }
-    };
+        }
+      };
 
     // logout
-    var logout = function(){
-      $location.path('/');
-    };
+      var logout = function(){
+        $window.localStorage.removeItem('jwtToken');
+        delete $window.localStorage['jwtToken'];
+        $location.path('/');
+      };
 
 
     // signup
-    var signup = function(user) {
-      var request= {
+      var signup = function(user) {
+        var request= {
         method : 'POST',
         url : baseUrl+'signup/newuser',
         data : user
-      }
+      };
       
       return $http(request).then(success, err);
       
@@ -53,106 +55,104 @@ angular
           $location.path('/login');
         }
         return resp;
-      };
+      }
 
       function err(err){
         $location.path('/');
         return console.log(err);
         }
-      };
+      }
 
-    function saveToken(token){
-      $window.localStorage['jwtToken'] = token;
-    }
+      function saveToken(token){
+        $window.localStorage['jwtToken'] = token;
+      }
 
-    function getToken(){
-      return $window.localStorage['jwtToken'];
-    }
+      function getToken(){
+        return $window.localStorage['jwtToken'];
+      }
      // get the event info from database 
     
 
-    var uploadDashboard = function() {
-      return $http({
-        method: 'GET',
-        url: 'http://localhost:8080/dashboard/upload',
-      })
-      .then(function(resp){
-        //console.log("data in uploadDashboard", resp.data)
-        return resp.data;
-      });
-    };
+      var uploadDashboard = function() {
+        return $http({
+          method: 'GET',
+          url: 'http://localhost:8080/dashboard/upload',
+        })
+        .then(function(resp){
+          //console.log("data in uploadDashboard", resp.data)
+          return resp.data;
+        });
+      };
 
 
     //// Twillio notification
-    var notify = function(sendText){
-      return $http({
-        method: 'POST',
-        url: 'http://localhost:8080/dashboard',
-        data: sendText
-      })
-      .then(function(data){
-        console.log("Sent the Messages", data);
-      })
-      .catch(function(err){
-        $location.path('/');
-        console.log(err);
-      })
-    };
+      var notify = function(sendText){
+        return $http({
+          method: 'POST',
+          url: 'http://localhost:8080/dashboard',
+          data: sendText
+        })
+        .then(function(data){
+          console.log("Sent the Messages", data);
+        })
+        .catch(function(err){
+          $location.path('/');
+          console.log(err);
+        })
+      };
 
        
        // new event request
-    var eventsPost = function(eventInfo) {
-      //console.log('eventinfo inside events post', eventInfo);
-      return $http({
-        method: 'POST',
-        url: 'http://localhost:8080/dashboard/events',
-        data: eventInfo
-      });
-
-    };
+      var eventsPost = function(eventInfo) {
+        //console.log('eventinfo inside events post', eventInfo);
+        return $http({
+          method: 'POST',
+          url: 'http://localhost:8080/dashboard/events',
+          data: eventInfo
+        });
+      };
 
        // get freinds list
-    var uploadFriendslist = function() {
-      return $http ({
-        method: 'GET',
-        url: 'http://localhost:8080/dashboard/friends'
-      });
-    };
+      var uploadFriendslist = function() {
+        return $http ({
+          method: 'GET',
+          url: 'http://localhost:8080/dashboard/friends'
+        });
+      };
 
 
      // add a record to database when user joins an event
-  var joinEvent = function(eventId) {
-     return $http({
-        method: 'POST',
-        url: 'http://localhost:8080/dashboard/join',
-        data: eventId
-      });
-  };
+      var joinEvent = function(eventId) {
+          return $http({
+            method: 'POST',
+            url: 'http://localhost:8080/dashboard/join',
+            data: eventId
+          });
+      };
 
 
      //remove the record of user from database// this isn't handled in the backend 
-  var unjoinEvent = function(userEventId) {
-      return $http({
-         method: 'POST',
-         url: 'http://localhost:8080/dashboard/unjoin',
-         data: userEventId
-      });
+      var unjoinEvent = function(userEventId) {
+          return $http({
+             method: 'POST',
+             url: 'http://localhost:8080/dashboard/unjoin',
+             data: userEventId
+          });
+      };
 
-  };
+      return {
+        login: login,
+        uploadDashboard: uploadDashboard,
+        notify: notify,
+        eventsPost: eventsPost,
+        signup: signup,
+        logout: logout,
+        username: username,
+        uploadFriendslist: uploadFriendslist,
+        joinEvent: joinEvent,
+        unjoinEvent: unjoinEvent
+      };
 
-    return {
-      login: login,
-      uploadDashboard: uploadDashboard,
-      notify: notify,
-      eventsPost: eventsPost,
-      signup: signup,
-      logout: logout,
-      username: username,
-      uploadFriendslist: uploadFriendslist,
-      joinEvent: joinEvent,
-      unjoinEvent: unjoinEvent
-    };
-
-});
+  });
 /// end of Services
 })();
