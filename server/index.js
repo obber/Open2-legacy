9a22f8536b73a03dd47a298724359b77510d3d2c
Ben <richterb3@gmail.com>
2016-04-19 19:49:14 -0700

    [Bugfix] Sign-in loads faster

--- server/index.js
+++ server/index.js
@@ -13,29 +13,18 @@ app.use(cors());
 router.post('/homepage', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
-  var hashedPass = bcrypt.hashSync(password, 10);
-  console.log(req.body);
-  var user = {username: username, password: password};
 
   auth(username, password).then(function(resp){
-    console.log('this is resp in auth func: ');
     var result = false;
-    resp.forEach(function(currentEl){
-      console.log('inside foreach');
-      console.log('inside if in foreach: ', currentEl.username, ' ', currentEl.password, ' ', bcrypt.hashSync(password, 10));
-
-      if(currentEl.username===username && bcrypt.compareSync(password, hashedPass)){
+    if(resp[0].username===username && bcrypt.compareSync(password, resp[0].password)){
         result = true;
       }
-    });
     if (result){
-      console.log('inside if');
       res.json({
           success : true,
           message : 'you are logged in'
       });
     } else {
-        console.log('inside else');
         res.json({
           success : false,
           message : 'invalid credentials'
@@ -44,30 +33,11 @@ router.post('/homepage', function(req, res){
   }).catch(function(err){
     console.log('unexpected error: ', err);
   });
-
-
-  // db.query('SELECT * FROM Users WHERE `username` = ?;', [username], function(err, rows) {
-  //   // console.log("This is our password in our db", rows[0].password)
-  //   // var hash = bcrypt.hashSync(password);
-
-  //   // console.log("This is the bcrypt pass true/false",bcrypt.compareSync( password ,rows[0].password ))
-
-  //   if (err) {
-  //     throw err;
-  //   } else {
-  //     if(!bcrypt.compareSync( password ,rows[0].password )){
-  //       console.log("Incorrect password");
-  //     }else{
-  //       res.send('/dashboard');
-  //     }
-  //   }
-  // })
-
 });
 
 function auth(user, pw){
   return new Promise(function(resolve, reject){
-    db.query('SELECT * FROM Users', function(err, query){
+    db.query('SELECT * FROM Users WHERE username = ?',[user],function(err, query){
       resolve(query);
     });
   });
