9443702a2da09ee15e0bc12372e55810fb723385
Kan Adachi <Kadachi1023@gmail.com>
2016-04-20 14:07:09 -0700

    Added spinner to login/signup

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -6,12 +6,16 @@ angular
   .controller('loginCtrl', function($scope, Services, $location) {
   
   $scope.pageClass = "login auth";
+  $scope.loading = false;
 
   $scope.redirectSignup = function(e) {
     $location.path('/signup');
   };
 
   $scope.submit = function() {
+
+    $scope.loading = true;
+
     var user = {
       username: $scope.username,
       password: $scope.password
