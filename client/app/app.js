a85d3c97d46dde790373a52cadd394530a8f0af3
Kan Adachi <Kadachi1023@gmail.com>
2016-04-21 09:18:36 -0700

    [Styles] angular templating setup

--- client/app/app.js
+++ client/app/app.js
@@ -7,17 +7,5 @@ angular
     'ngMessages'
     ])
 
-
-//moved to dashboard controller
-// function DialogController($scope, $mdDialog) {
-//   $scope.hide = function() {
-//     $mdDialog.hide();
-//   };
-//   $scope.cancel = function() {
-//     $mdDialog.cancel();
-//   };
-//   $scope.answer = function(answer) {
-//     $mdDialog.hide(answer);
-//   };
-// }
 })();
+
