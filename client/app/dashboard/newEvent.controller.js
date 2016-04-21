cbab667c2af54488c06f67f0a92e2575f39ed46c
Kan Adachi <Kadachi1023@gmail.com>
2016-04-20 20:15:12 -0700

    [Styles] backdrop opacity transition fixed

---
+++ client/app/dashboard/newEvent.controller.js
@@ -0,0 +1,13 @@
+(function() { 
+
+angular
+  .module('myApp')
+  // dashboard controller
+  .controller('newEventCtrl', newEventCtrl);
+
+  function newEventCtrl() {
+
+    
+  }
+
+})(); ///////// end of dahboard controller
